f022e6e608341dae59dc4fa01b33906a3172cd70
Anhelina Mukha <s20987@pjwstk.edu.pl>
2025-11-02 09:40:31 +0100

    Homework annotations 6th lesson fix

--- L06-annotations/src/main/java/ru/otus/myannotation/Main.java
+++ L06-annotations/src/main/java/ru/otus/myannotation/Main.java
@@ -5,45 +5,9 @@ import java.util.ArrayList;
 
 @SuppressWarnings("java:S106")
 public class Main {
-    static Class<MyTests> clazz = MyTests.class;
 
     public static void main(String[] args) {
-        ArrayList<Method> befores = new ArrayList<>();
-        ArrayList<Method> tests = new ArrayList<>();
-        ArrayList<Method> afters = new ArrayList<>();
-
-        for (Method method : clazz.getDeclaredMethods()) {
-            if (method.isAnnotationPresent(Before.class)) {
-                befores.add(method);
-            }
-            if (method.isAnnotationPresent(Test.class)) {
-                tests.add(method);
-            }
-            if (method.isAnnotationPresent(After.class)) {
-                afters.add(method);
-            }
-        }
-
-        for (Method test : tests) {
-            runAllMethods(befores);
-            executeMethod(test);
-            runAllMethods(afters);
-        }
+        TestsRunnerUtil.runTests(MyTests.class);
     }
 
-    static void runAllMethods(ArrayList<Method> methods) {
-        for (Method method : methods) {
-            executeMethod(method);
-        }
-    }
-
-    @SuppressWarnings("java:S112")
-    private static void executeMethod(Method method) {
-        try {
-            Object instance = clazz.getDeclaredConstructor().newInstance();
-            method.invoke(instance);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
